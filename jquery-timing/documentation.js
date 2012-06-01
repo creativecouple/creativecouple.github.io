e036c3a8a672824bde3b54ddbe8966c790a6f124
peter <peter.liske@gmx.net>
2012-06-01 10:21:34 +0200

    make the bot-safe email address human clickable

--- jquery-timing/documentation.js
+++ jquery-timing/documentation.js
@@ -4,11 +4,22 @@ $(document).ready(function(){
 				tic: 'Timed Invocation Chain',
 				fxq: 'Effects Queue'
 		}
-	
 		$('abbr').each(function(){
 			var $this = $(this);
 			$this.attr('title', $this.attr('title') || abbreviation_map[$this.text()]);
 		});
 		
+		// make tables look nice		
 		$('tr:odd').addClass('odd');
+		
+		// make address chaos clickable
+		$('address').click(function(){
+			var targetAddress = [];
+			$(this).children().each(function(){
+				$.each($(this).text(), function(i){
+					targetAddress[i] = (this == ' ') ? targetAddress[i] : this;
+				});
+			});
+			window.location = 'mailto:'+targetAddress.join('');
+		});
 });
